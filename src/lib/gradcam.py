aece73b538dbcbaa98c5ef1e64e28ed115d041de
Daiki.Sanno <daiki.sanno@gmail.com>
2017-11-05 23:20:53 +0900

    Do not access _variable of VariableNode.

--- src/lib/gradcam.py
+++ src/lib/gradcam.py
@@ -8,14 +8,45 @@ from chainer import function_hook
 from chainer import cuda
 
 
+def _call_is_matched(function_node, call, node_to_item):
+    if isinstance(call, chainer.Link):
+        return call == node_to_item[function_node]['link']
+    return call == node_to_item[function_node]['function']
+
+
+def _sequence_is_matched(function_node, call_sequence, node_to_item):
+    last = call_sequence[-1]
+    if not _call_is_matched(function_node, last, node_to_item):
+        return False
+    init = call_sequence[:-1]
+    if init == []:
+        return True
+    for x in function_node.inputs:
+        if x.creator_node is None:
+            continue
+        if _sequence_is_matched(x.creator_node, init, node_to_item):
+            return True
+    return False
+
+
 class CallerInspectionHook(function_hook.FunctionHook):
 
     name = 'CallerInspectionHook'
 
-    def __init__(self):
+    def __init__(self, call_sequence):
+        self._call_sequence = call_sequence
         self._call_history = []
+        self._node_to_item = {}
+        self._matched = []
 
     def forward_postprocess(self, function, in_data):
+        if self._call_history != []:
+            function_node = self._call_history[-1]['function_node']
+            if _sequence_is_matched(function_node, self._call_sequence, self._node_to_item):
+                outputs = [output() for output in function_node.outputs]
+                if all([output is not None for output in outputs]):
+                    outputs = [output.get_variable() for output in outputs]
+                    self._matched.append((function_node, outputs))
         stack = inspect.stack()
         function_node = stack[1][0].f_locals['self']
         # function that calls "apply" of FunctionNode instance
@@ -29,46 +60,29 @@ class CallerInspectionHook(function_hook.FunctionHook):
                 if isinstance(caller_instance, chainer.Link):
                     link = caller_instance
                     break
-        self._call_history.append({
+        history_item = {
             'function_node': function_node,
             'function': function,
             'link': link,
-        })
+        }
+        self._call_history.append(history_item)
+        self._node_to_item[function_node] = history_item
 
     @property
-    def call_history(self):
-        return self._call_history
+    def matched(self):
+        return self._matched
 
 
-def _call_is_matched(function_node, call, node_to_item):
-    if isinstance(call, chainer.Link):
-        return call == node_to_item[function_node]['link']
-    return call == node_to_item[function_node]['function']
-
-
-def _sequence_is_matched(function_node, call_sequence, node_to_item):
-    head = call_sequence[0]
-    if not _call_is_matched(function_node, head, node_to_item):
+def _graph_has_function_node(root_variable, target):
+    if root_variable.creator_node is None:
         return False
-    tail = call_sequence[1:]
-    if tail == []:
+    function_node = root_variable.creator_node
+    if function_node == target:
         return True
     for x in function_node.inputs:
-        if x.creator_node is None:
-            continue
-        if _sequence_is_matched(x.creator_node, tail, node_to_item):
+        if _graph_has_function_node(x, target):
             return True
     return False
-
-
-def _find_call_sequence(call_history, call_sequence):
-    reversed_sequence = list(reversed(call_sequence))
-    node_to_item = {item['function_node']: item for item in call_history}
-    for i, history_item in enumerate(call_history):
-        function_node = history_item['function_node']
-        if _sequence_is_matched(function_node, reversed_sequence, node_to_item):
-            return i, function_node
-    return -1, None
 
 
 def gradcam(model, x, calls, y_grad=None, loss_func=None, feature_index=0):
@@ -77,17 +91,19 @@ def gradcam(model, x, calls, y_grad=None, loss_func=None, feature_index=0):
     else:
         xp = cuda.get_array_module(x)
 
-    with CallerInspectionHook() as hook:
+    with CallerInspectionHook(calls) as hook:
         with chainer.using_config('train', False):
             if loss_func is not None:
                 y = loss_func(x)
             else:
                 y = model(x)
-        index, function_node = _find_call_sequence(hook.call_history, calls)
-        # store variables because their gradients vanishes without reference to them
-        h_node = function_node.outputs[feature_index]()
-        h = h_node.get_variable()
-        h_node._variable = weakref.ref(h)
+        h = None
+        for function_node, outputs in hook.matched:
+            if _graph_has_function_node(y, function_node):
+                h = outputs[feature_index]
+
+    if h is None:
+        return None
 
     model.cleargrads()
     y.grad = y_grad
